11ffa127d6bd98db48057ca2403d77ff0b8cbb4e
KellenNjoroge <muthonkel@gmail.com>
2018-10-12 10:39:08 +0300

    auth.html

---
+++ app/main/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SubmitField, SelectField
+from wtforms.validators import Required
+
+
+class PitchForm(FlaskForm):
+    pitch = TextAreaField('Your Pitch', validators=[Required()])
+    my_category = SelectField('Category',
+                              choices=[('Interview-Pitch', 'Interview Pitch'), ('Product-Pitch', 'Product Pitch'),
+                                       ('Promotion-Pitch', 'Promotion Pitch'), ('Business', 'Business'),
+                                       ('Academic', 'Academic'), ('Political', 'Political'),
+                                       ('Technology', 'Technology'), ('Health', 'Health') ,('pick-up-lines', 'pick-up-lines')], validators=[Required()])
+    submit = SubmitField('Pitch😄!')
+
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Comment', validators=[Required()])
+    submit = SubmitField('Post Comment')
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Write something about yourself', validators=[Required()])
+    submit = SubmitField('Submit')
